8493f94d11412c3e95214dcb2933d1a3f5fccb83
cccs-eric <eric.ladouceur@cyber.gc.ca>
2021-01-20 12:24:07 +0000

    Switched CCCS build to uchimera CR
    Signed-off-by: cccs-eric <eric.ladouceur@cyber.gc.ca>

--- cccs-build.sh
+++ cccs-build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 VERSION=$(cat VERSION)
+#REGISTRY=cranalyticalplatform.azurecr.io
+REGISTRY=uchimera.azurecr.io
 
-./build.sh --registry cranalyticalplatform.azurecr.io --organization cccs --tag ${VERSION} --offline
+./build.sh --registry ${REGISTRY} --organization cccs --tag ${VERSION} --offline
 if [ $? -ne 0 ]; then
 	echo "Failed to build docker image"
 	exit 1
 fi
-docker push cranalyticalplatform.azurecr.io/cccs/che-devfile-registry:${VERSION}
+docker push ${REGISTRY}/cccs/che-devfile-registry:${VERSION}
 
