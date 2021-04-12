853486d7e7a5e0fe0409771e0fe635da494e6d13
cccs-eric <eric.ladouceur@cyber.gc.ca>
2021-04-12 18:20:15 +0000

    Updated azure pipeline

--- azure/portkey.py
+++ azure/portkey.py
@@ -12,28 +12,26 @@ def extractRepository(line) -> dict:
 
 def initArgparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
-        usage="%(prog)s [OPTION] [FILE]",
+        usage="%(prog)s [OPTION] <extImagesFile> <portKeyOuputFile>",
         description="Creates a PortKey configuration from an Eclipse Che devfile registry external_images.txt file."
     )
     parser.add_argument(
         "-v", "--version", action="version",
         version = f"{parser.prog} version 1.0.0"
     )
-    parser.add_argument('extImages', nargs='*')
+    parser.add_argument('extImagesFile')
+    parser.add_argument('portKeyOuputFile')
     return parser
 
 def main() -> None:
     parser = initArgparse()
     args = parser.parse_args()
-    if not args.extImages:
-        parser.print_usage()
-        sys.exit(1)
     print("Start of portkey config creation")
     templateLoader = FileSystemLoader(searchpath=os.path.join(os.path.dirname(__file__), "./templates/"))
     templateEnv = Environment(loader=templateLoader)
     template = templateEnv.get_template("portkey_template.j2")
     images = []
-    with open("/home/erladou/git/cccs/che-devfile-registry/external_images.txt", "r") as extImgFile:
+    with open(args.extImagesFile, 'r') as extImgFile:
         for line in extImgFile:
             try:
                 images.append(extractRepository(line.strip()))
@@ -44,7 +42,8 @@ def main() -> None:
     quayImages['repositories'] = [img for img in images if img['registry'] == 'quay.io']
     crImages = [img for img in images if img['registry'] == 'cranalyticalplatform.azurecr.io']
     
-    print(template.render(quayImages))
+    with open(args.portKeyOuputFile, 'w') as portKeyOutput:
+        portKeyOutput.write(template.render(quayImages))
     #print(quayImages)
 
 if __name__ == "__main__":
